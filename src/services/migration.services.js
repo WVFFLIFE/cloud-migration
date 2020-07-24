abeb2a1a5b52f93ab3137dc7d62d24e4d6a35cd6
Фесюк Ярослав <noobfrizz1@gmail.com>
2020-07-24 16:50:39 +0300

    init

--- src/services/migration.services.js
+++ src/services/migration.services.js
@@ -1,8 +1,10 @@
+/** @format */
+
 import authentication from '../b2c';
 
 async function request(uri, method = 'GET', body = null) {
   const token = await authentication.getAccessToken();
-  const postOptions = body ? {'body': JSON.stringify(body)} : {}
+  const postOptions = body ? { body: JSON.stringify(body) } : {};
 
   return await fetch(uri, {
     method,
@@ -11,11 +13,11 @@ async function request(uri, method = 'GET', body = null) {
       'Content-Type': 'application/json'
     },
     ...postOptions
-  })
+  });
 }
 
 class MigrationService {
-  BASE_URL = `${window.location.origin}/api`
+  BASE_URL = 'http://localhost:44310/api'; //`${window.location.origin}/api`
 
   get = async path => {
     const uri = `${this.BASE_URL}${path}`;
@@ -23,11 +25,11 @@ class MigrationService {
     const res = await request(uri);
 
     if (!res.ok) {
-      throw new Error(`Couldn't fetch ${uri}`)
+      throw new Error(`Couldn't fetch ${uri}`);
     }
 
     return await res.json();
-  }
+  };
 
   put = async (path, body) => {
     const uri = `${this.BASE_URL}${path}`;
@@ -35,81 +37,80 @@ class MigrationService {
     const res = await fetch(uri, {
       method: 'PUT',
       headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
       },
       body: JSON.stringify(body)
-    })
+    });
 
     if (!res.ok) {
-      throw new Error(`Couldn't fetch ${uri}`)
+      throw new Error(`Couldn't fetch ${uri}`);
     }
 
     return await res.json();
-  }
+  };
 
   post = async (path, body) => {
     const uri = `${this.BASE_URL}${path}`;
 
-    const res = await request(uri, 'POST', body)
+    const res = await request(uri, 'POST', body);
 
     if (!res.ok) {
-      throw new Error(`Couldn't fetch ${uri}`)
+      throw new Error(`Couldn't fetch ${uri}`);
     }
 
     return await res.json();
-  }
+  };
 
   postStep = async (path, body) => {
     const uri = `${this.BASE_URL}${path}`;
 
-    const res = await request(uri, 'POST', body)
+    const res = await request(uri, 'POST', body);
 
     if (!res.ok) {
-      throw new Error(`Couldn't fetch ${uri}`)
+      throw new Error(`Couldn't fetch ${uri}`);
     }
 
     return true;
-  }
+  };
 
-
-  delete = async (path) => {
+  delete = async path => {
     const uri = `${this.BASE_URL}${path}`;
 
-    const res = await request(uri, 'DELETE')
+    const res = await request(uri, 'DELETE');
 
     if (!res.ok) {
-      throw new Error(`Couldn't delete ${uri}`)
+      throw new Error(`Couldn't delete ${uri}`);
     }
 
     return true;
-  }
+  };
 
   validate = async (path, body) => {
     const uri = `${this.BASE_URL}${path}`;
 
-    const res = await request(uri, 'POST', body)
+    const res = await request(uri, 'POST', body);
 
     if (!res.ok) {
       try {
-        const {message} = await res.json();
+        const { message } = await res.json();
 
         return {
           status: 'error',
           message
-        }
+        };
       } catch {
-        throw new Error(`Couldn't fetch ${uri}`)
+        throw new Error(`Couldn't fetch ${uri}`);
       }
     }
 
-    const {message} = await res.json();
+    const { message } = await res.json();
 
     return {
       status: 'success',
       message
     };
-  }
+  };
 }
 
 export default new MigrationService();
