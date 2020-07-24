3a84ae2416bd6c12a162be26e675f685338f36e7
wvfflife <botmailpas@yahoo.com>
2020-07-24 12:30:30 +0300

    update redirect uri

--- src/config/authentication.js
+++ src/config/authentication.js
@@ -9,7 +9,7 @@ authentication.initialize({
   // the policy to use to sign in, can also be a sign up or sign in policy
   signInPolicy: 'B2C_1_signupsignin',
   // the the B2C application you want to authenticate with (that's just a random GUID - get yours from the portal)
-  clientId: '0497bb81-2c80-4419-b857-5bf651bb4e88',
+  clientId: '150fbaaa-2633-4d3a-8422-79a4421cf864',
   // where MSAL will store state - localStorage or sessionStorage
   cacheLocation: 'localStorage',
   // the scopes you want included in the access token
